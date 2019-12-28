0de37777a5aad28b54893b9694e92f52f6c61507
colman423 <colman0423@gmail.com>
2019-12-28 21:51:38 +0800

    correct cube change

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,7 +6,7 @@ public class GameManager : MonoBehaviour
   private Move m_Move;
 
 
-  public Stage[] cubeList;
+  public Stage[] stageList;
   public int nowStageNo = -1;
   private Stage nowStage;
   private int nowStageStep;
@@ -29,7 +29,7 @@ public class GameManager : MonoBehaviour
   public void goNextStage()
   {
     nowStageNo++;
-    nowStage = cubeList[nowStageNo];
+    nowStage = stageList[nowStageNo];
     nowStageStep = nowStage.cubes.Length;
     nowStageCorrectDirection = nowStage.correctDirection;
     foreach (GameObject cube in nowStage.cubes)
@@ -55,27 +55,6 @@ public class GameManager : MonoBehaviour
     int step = nowStageCorrectDirection == DIRECTION.FRONT ? nowStageStep : wrongStep;
     m_Move.move(DIRECTION.FRONT, step);
 
-  }
-
-
-  public GameObject cube;
-
-
-
-
-  public GameObject column;
-
-  public void dev()
-  {
-    cube.GetComponent<TouchCube>().collisionEnabled = true;
-    // foreach (Transform cube in column.transform)
-    // {
-    //   string name = cube.name;
-    //   name = name.Split('(')[1];
-    //   name = name.Split(')')[0];
-    //   int no = Int32.Parse(name);
-    //   cube.localPosition = new Vector3(0, 0, 50-no);
-    // }
   }
 
 }
